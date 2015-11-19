72cd3399705b1bc9d265afcfd0e7b6864a134571
Cronverk <pozniack@gmail.com>
2015-11-19 15:42:16 +0200

    0.0.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 /**
  * Created by Grass on 17.11.2015.
  */
+$(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > 200) {
+        $('.navbar-default').css('display', 'block');
+        $('.navbar-default').addClass('fixed-to-top');
+    } else if (scrollTop == 0) {
+        $('.navbar-default').removeClass('fixed-to-top');
+    }
+});
+var buttons = document.getElementsByClassName("info-button");
+var click = function click(v){
+    var child =this.offsetParent.firstElementChild.lastElementChild;
+
+    if(child.style.display === "none"|| child.style.display === "")
+        child.style.display = "inline-block";
+    else
+        child.style.display ="none";
+}
+
+for(var i = 0 ; i < buttons.length;i++ )
+    buttons[i].onclick  = click;
+
+
